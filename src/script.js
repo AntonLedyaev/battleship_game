d6c392be569e1fe5d6524b3dd3e966b9a80a5911
Anton Ledyaev <9787@mail.ru>
2022-01-15 18:46:26 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const battlefield = new BattlefieldView;
+
+console.log(battlefield);
+
+/*
+const mouse = new Mouse(document.body);
+
+requestAnimationFrame(tick);
+
+function tick() {
+    requestAnimationFrame(tick);
+
+    console.log(mouse.left, mouse.pLeft);
+
+    mouse.tick();
+} */
